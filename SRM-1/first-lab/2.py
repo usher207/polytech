bc8d5260b9318fe6dfd49b47acd7cabb53079d89
Klysha Maksym <usher2070323@gmail.com>
2024-09-16 12:52:21 +0200

    Add input validation to tasks

--- SRM-1/first-lab/2.py
+++ SRM-1/first-lab/2.py
@@ -1,5 +1,18 @@
 import math
-a = int(input("Введіть додатне значення a: "))
-b = int(input("Введіть додатне значення b: "))
+import math
+
+def get_positive_integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value > 0:
+                return value
+            else:
+                print("Будь ласка, введіть додатне число.")
+        except ValueError:
+            print("Невірний ввід. Спробуйте ще раз.")
+
+a = get_positive_integer("Введіть додатне значення a: ")
+b = get_positive_integer("Введіть додатне значення b: ")
 с = math.sqrt(a * b)
 print(f"Середнє геометричне чисел {a} та {b} = {с}")
--- SRM-1/first-lab/9.py
+++ SRM-1/first-lab/9.py
@@ -1,13 +1,21 @@
-N = int(input("Введіть ціле число N (N > 0): "))
+def get_positive_integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value > 0:
+                return value
+            else:
+                print("Помилка: число повинно бути більше 0.")
+        except ValueError:
+            print("Невірний ввід. Будь ласка, введіть ціле число.")
 
-if N > 0:
-    reversed_N = 0 
-    
-    while N > 0:
-        last_digit = N % 10  
-        reversed_N = reversed_N * 10 + last_digit  
-        N = N // 10  
-        
-    print(f"reversed N: {reversed_N}")
-else:
-    print("error: N < or = 0")
+N = get_positive_integer("Введіть ціле число N (N > 0): ")
+
+reversed_N = 0
+
+while N > 0:
+    last_digit = N % 10
+    reversed_N = reversed_N * 10 + last_digit
+    N = N // 10
+
+print(f"Зворотне число: {reversed_N}")
